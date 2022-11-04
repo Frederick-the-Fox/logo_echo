e74fc1b7579a5581fb8fb5719b6477339b9af28f
wyc001208 <wyc20001208@126.com>
2022-11-04 16:56:53 +0800

    调整换行策略为-e -n

---
+++ logo_echo/logoshow.sh
@@ -0,0 +1,264 @@
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # Y
+echo -e "\033[1;33m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m_\033[0m" # R
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;31m/\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;31m/\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;31m/\033[0m" # Y
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;31m/\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m/\033[0m\c" # R
+# \_ Y
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;33m \033[0m\c" # blank
+# \_ Y
+echo -e -n "\033[1;31m\\" # Y\
+echo -e "\033[1;31m_\033[0m\c" # Y
+echo -e "\033[1;31m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m/\033[0m" # R
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[0;37mall \033[0m\c" # all
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# \_ R
+echo -e -n "\033[1;31m\\" # R\
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m_\033[0m\c" # R
+# _/ R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# \_ R
+echo -e -n "\033[1;31m\\" # R\
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m_\033[0m\c" # R
+# _/ R
+echo -e "\033[1;31m_\033[0m\c" # R
+echo -e "\033[1;31m/\033[0m" # R
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m/\033[0m\c" # Y
+echo -e "\033[0;37mrights \033[0m\c" # all
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank 
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# \_ R
+echo -e -n "\033[1;32m\\" # R\
+echo -e "\033[1;32m_\033[0m\c" # R
+echo -e "\033[1;32m \033[0m\c" # blank
+# \_ R
+echo -e -n "\033[1;32m\\" # R\
+echo -e "\033[1;32m_\033[0m\c" # R
+# _/ R
+echo -e "\033[1;32m_\033[0m\c" # R
+echo -e "\033[1;32m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;32m_\033[0m\c" # R
+echo -e "\033[1;32m/\033[0m" # R
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m/\033[0m\c" # R
+echo -e "\033[0;37mreserved \033[0m\c" # all
+echo -e "\033[1;33m|     \033[0m\c" # Y
+# \_ R
+echo -e -n "\033[1;32m\\" # R\
+echo -e "\033[1;32m_\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ R
+echo -e "\033[1;32m_\033[0m\c" # R
+echo -e "\033[1;32m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;31m✿\033[0m" # R
+
+
+echo -e "\033[1;33m  | \033[0m\c" # Y
+echo -e "\033[1;37mWangYC  \033[0m\c" # all
+echo -e "\033[1;33m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ R
+echo -e "\033[1;34m_\033[0m\c" # B
+echo -e "\033[1;32m/\033[0m\c" # R
+echo -e "\033[1;32m_\033[0m\c" # B
+echo -e "\033[1;32m_\033[0m\c" # B
+echo -e "\033[1;32m/\033[0m\c" # R
+echo -e "\033[1;34m_ _ _\033[0m\c" # G
+echo -e "\033[0;32m\`|'\033[0m" # G
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # R\
+echo -e "\033[0;37m  2022  \033[0m\c" # all
+echo -e "\033[1;33m/\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ R
+echo -e "\033[1;34m_\033[0m\c" # R
+echo -e "\033[1;34m/\033[0m\c" # R
+echo -e "\033[1;34m _ _ _ _ \033[0m\c" # R
+echo -e "\033[1;34m_\033[m\c"
+echo -e "\033[1;34m/\033[m"
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # Y\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[0;37m© \033[0m\c" # all
+echo -e "\033[1;33m/\033[m\c"
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ B
+echo -e "\033[1;34m_\033[m\c"
+echo -e "\033[1;34m/\033[m\c"# _/ B
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;34m_\033[m\c"
+echo -e "\033[1;34m/\033[m"
+
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # R\
+echo -e "\033[1;33m_\033[0m\c" # R
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e -n "\033[1;33m\\" # R\
+echo -e "\033[1;33m \033[0m\c" # blank
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ B
+echo -e "\033[1;34m_\033[m\c"
+echo -e "\033[1;34m/\033[m\c"
+echo -e "\033[1;33m \033[0m\c" # blank
+# _/ B
+echo -e "\033[1;34m_\033[m\c"
+echo -e "\033[1;34m/\033[m\c"
+echo -e "\033[1;34m_ _ _ _ _\033[0m" # B
+
+echo -e "\033[1;33m       \033[0m\c" # blank
+# \_ R
+echo -e -n "\033[1;33m\\" # R\
+echo -e "\033[1;33m_\033[0m\c" # R
+# \_ R
+echo -e -n "\033[1;33m\\" # R\
+echo -e "\033[1;33m_\033[0m\c" # R
+echo -e "\033[1;34m/\033[m\c"
+echo -e "\033[1;34m_ _ _ _ _ _ _/\033[0m" # B
